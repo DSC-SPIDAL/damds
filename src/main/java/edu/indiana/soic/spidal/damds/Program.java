7251a927f8195e921e0b5c332d7a4c4dbfd730ff
Saliya Ekanayake <esaliya@gmail.com>
2015-06-25 14:51:42 -0400

    Adding a a test to read the same input file to compare with Twister for the code in Master branch

--- src/main/java/edu/indiana/soic/spidal/damds/Program.java
+++ src/main/java/edu/indiana/soic/spidal/damds/Program.java
@@ -762,6 +762,35 @@ public class Program {
 
     static double[][] generateInitMapping(int numDataPoints,
                                           int targetDim) {
+        /* TODO remove after testing */
+        String file = "/N/u/sekanaya/sali/projects/salsabio/phy/updated_4.20.15/mds/initprex.txt";
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(file),
+                                                         Charset.defaultCharset())){
+            double x[][] = new double[numDataPoints][targetDim];
+            String line;
+            Pattern pattern = Pattern.compile("[\t ]");
+            int row = 0;
+            while ((line = br.readLine()) != null) {
+                if (Strings.isNullOrEmpty(line))
+                    continue; // continue on empty lines - "while" will break on null anyway;
+
+                String[] splits = pattern.split(line.trim());
+
+                for (int i = 0; i < splits.length; ++i){
+                    if (i == 0){
+                        splits[i] = splits[i].substring(1);
+                    }
+                    splits[i] = splits[i].substring(0,splits[i].length() -1);
+                    x[row][i] = Double.parseDouble(splits[i]);
+                }
+                ++row;
+            }
+            return x;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }/*
         double matX[][] = new double[numDataPoints][targetDim];
         // Use Random class for generating random initial mapping solution.
         Random rand = new Random(System.currentTimeMillis());
@@ -773,7 +802,7 @@ public class Program {
                     matX[i][j] = -rand.nextDouble();
             }
         }
-        return matX;
+        return matX;*/
     }
 
     private static DoubleStatistics calculateStatistics(
