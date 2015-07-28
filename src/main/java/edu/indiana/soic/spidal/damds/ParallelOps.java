5c9524f5cc9b3d0346887c3ef1cef7ff32be1876
Saliya Ekanayake <esaliya@gmail.com>
2015-07-28 15:45:22 -0400

    Adding some debugs

--- src/main/java/edu/indiana/soic/spidal/damds/ParallelOps.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ParallelOps.java
@@ -109,7 +109,7 @@ public class ParallelOps {
                                + "\nthreadIdx: " + 0 + " tRowCount: " + ParallelOps.threadRowCounts[0]
                                + " tPointStartOffset: " + ParallelOps.threadPointStartOffsets[0]
                                + "\ni: " + 0
-                               + "\nprocRowStartOffset: " + ParallelOps.procRowStartOffset + " procRowStartOffset(long): " + (((long)procRowStartOffset)*globalColCount) + " procPointStartOffset: " + ParallelOps.procPointStartOffset + " procLocalPnum: "
+                               + "\nprocRowStartOffset: " + ParallelOps.procRowStartOffset + " procPointStartOffset(long): " + (((long)procRowStartOffset)*globalColCount) + " procPointStartOffset: " + ParallelOps.procPointStartOffset + " procLocalPnum: "
                                + "\nglobalColCount: " + globalColCount);
         }
         System.exit(0);
