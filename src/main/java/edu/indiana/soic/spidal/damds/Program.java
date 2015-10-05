2dac13034c9a204441227a2f88ec9f3210cb71f2
Saliya Ekanayake <esaliya@gmail.com>
2015-10-04 20:57:07 -0400

    Code Optimization: new 16 - new 15 showed that whatever you write to threadPartialMM is correctly collected and written to output, so now let's see what happens if we give a fresh array to BofZ and use that to do MM and collect results.

--- src/main/java/edu/indiana/soic/spidal/damds/Program.java
+++ src/main/java/edu/indiana/soic/spidal/damds/Program.java
@@ -1084,14 +1084,16 @@ public class Program {
         short[][] distances, WeightsWrap weights, int blockSize,
         float[][] outBofZ, double[][] outMM) {
 
-        // TODO - testing
+        /*// TODO - testing
         for (int i = 0; i < ParallelOps.threadRowCounts[threadIdx]; ++i){
             outMM[i][0] = ParallelOps.worldProcRank;
             outMM[i][1] = threadIdx;
             outMM[i][2] = 0.1375;
-        }
+        }*/
 
-        /*BCInternalTimings.startTiming(BCInternalTimings.TimingTask.BOFZ, threadIdx);
+        outBofZ = new float[ParallelOps.threadRowCounts[threadIdx]][ParallelOps.globalColCount];
+
+        BCInternalTimings.startTiming(BCInternalTimings.TimingTask.BOFZ, threadIdx);
         calculateBofZ(threadIdx, preX, targetDimension, tCur,
                                         distances, weights, outBofZ);
         BCInternalTimings.endTiming(BCInternalTimings.TimingTask.BOFZ, threadIdx);
@@ -1100,7 +1102,7 @@ public class Program {
         BCInternalTimings.startTiming(BCInternalTimings.TimingTask.MM, threadIdx);
         MatrixUtils.matrixMultiply(outBofZ, preX, ParallelOps.threadRowCounts[threadIdx],
                                                   targetDimension, ParallelOps.globalColCount, blockSize, outMM);
-        BCInternalTimings.endTiming(BCInternalTimings.TimingTask.MM, threadIdx);*/
+        BCInternalTimings.endTiming(BCInternalTimings.TimingTask.MM, threadIdx);
     }
 
     private static void calculateBofZ(
