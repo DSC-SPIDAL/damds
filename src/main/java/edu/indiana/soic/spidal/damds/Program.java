f097cfc5ecd55fb3d4d826d6755856b9face8912
Saliya Ekanayake <esaliya@gmail.com>
2016-02-05 14:29:10 -0500

    Fixing bug with statistics

--- src/main/java/edu/indiana/soic/spidal/damds/Program.java
+++ src/main/java/edu/indiana/soic/spidal/damds/Program.java
@@ -1474,7 +1474,7 @@ public class Program {
         for (int localRow = 0; localRow < threadRowCount; ++localRow){
             procLocalRow = localRow + threadRowStartOffset;
             for (int globalCol = 0; globalCol < ParallelOps.globalColCount; globalCol++) {
-                origD = distances[localRow*ParallelOps.globalColCount + globalCol] * INV_SHORT_MAX;
+                origD = distances[procLocalRow*ParallelOps.globalColCount + globalCol] * INV_SHORT_MAX;
                 weight = weights.getWeight(procLocalRow,globalCol);
                 if (origD < 0) {
                     // Missing distance
