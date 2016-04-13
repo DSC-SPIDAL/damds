31414b075f2f90b242cef11e8c6dab6c4abad479
supunkamburugamuva <supun06@gmail.com>
2016-04-13 14:19:22 -0400

    adding busy threads

--- src/test/java/edu/indiana/soic/spidal/threads/SpidalThreadTest.java
+++ src/test/java/edu/indiana/soic/spidal/threads/SpidalThreadTest.java
@@ -6,7 +6,17 @@ import junit.framework.TestCase;
 public class SpidalThreadTest extends TestCase {
 
     public void testA() {
-        // SpidalThreads threads = new SpidalThreads(2, true, true, 4, 1);
-        // threads.forall((threadId) -> System.out.println(threadId));
+        SpidalThreads threads = new SpidalThreads(10, false, false, 48, 1);
+        threads.forall((threadId) -> print(threadId));
+        System.out.println("Finished");
+    }
+
+    public void print(int thread) {
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                int k = i / (j + 1);
+            }
+        }
+        System.out.println(thread);
     }
 }
