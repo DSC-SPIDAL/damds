973eec8fbad268df9b3e4f9ec89911bf18264219
Saliya Ekanayake <esaliya@gmail.com>
2016-04-15 10:50:52 -0400

    adding debugs

--- src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
@@ -209,7 +209,7 @@ public class ProgramWorker {
                     threadComm.barrier();
 
                     // TODO - debugs
-                    System.out.println("Rank: " + ParallelOps.worldProcRank + " Tid: " + threadId + " afterBC preX[2600][1]: " + preX[2600*3+1] + "preX[7200][2]: " + preX[7200*3+2]);
+                    System.out.println("Rank: " + ParallelOps.worldProcRank + " Tid: " + threadId + " afterBC preX[2600][1]: " + preX[2600*3+1] + " preX[7200][2]: " + preX[7200*3+2]);
 
                     stressLoopTimings.startTiming(
                         StressLoopTimings.TimingTask.CG);
@@ -535,6 +535,9 @@ public class ProgramWorker {
             ParallelOps.worldProcsComm.barrier();
         }
         threadComm.barrier();
+
+        // TODO - debugs
+        System.out.println("Rank: " + ParallelOps.worldProcRank + " Tid: " + threadId + " inCG after 1st MM MMr[2600][1]: " + MMr[2600*3+1] + " MMr[7200][2]: " + MMr[7200*3+2]);
 
         int iOffset;
 
