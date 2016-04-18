62518a56ae6b38914b55f4969b4f4d4c767a2f64
Saliya Ekanayake <esaliya@gmail.com>
2016-04-18 14:21:27 -0400

    with HJ bound threads

--- src/main/java/edu/indiana/soic/spidal/damds/ProgramLRT.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ProgramLRT.java
@@ -112,13 +112,13 @@ public class ProgramLRT {
                         0, ParallelOps.threadCount - 1,
                         (threadIdx) -> {
 
-                            BitSet bitSet = new BitSet(48);
+                           /* BitSet bitSet = new BitSet(48);
                             // TODO - let's hard code for juliet for now
                             bitSet.set((ParallelOps.worldProcRank * 12) +
                                     threadIdx + 1);
                             bitSet.set((ParallelOps.worldProcRank * 24) +
-                                    threadIdx + 1 + 24);
-                            Affinity.setAffinity(bitSet);
+                                    threadIdx + 1 + 24);*/
+                            Affinity.setAffinity(1);
 
                             final ProgramWorker worker = new ProgramWorker
                                     (threadIdx,
