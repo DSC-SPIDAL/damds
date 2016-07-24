df48a314c6f9931e834a5367d46494da91d0b7ed
Saliya Ekanayake <esaliya@gmail.com>
2016-07-23 21:56:14 -0400

    debugs: checking why jna is not loaded on 36 core nodes

--- src/main/java/edu/indiana/soic/spidal/damds/ProgramLRT.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ProgramLRT.java
@@ -125,8 +125,7 @@ public class ProgramLRT {
                         (threadIdx) -> {
 
                             if (bind) {
-                                BitSet bitSet = ThreadBitAssigner.getBitSet(ParallelOps.worldProcRank, threadIdx, ParallelOps.threadCount, cps);
-                                Affinity.setAffinity(bitSet);
+
                                 try {
                                     if (ParallelOps.worldProcRank == 1){
                                         System.out.println("Rank: " + ParallelOps.worldProcRank + " os.name " + System.getProperty("os.name") + " " + Affinity.getAffinityImpl().getClass());
@@ -143,6 +142,10 @@ public class ProgramLRT {
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
+
+//                                BitSet bitSet = ThreadBitAssigner.getBitSet(ParallelOps.worldProcRank, threadIdx, ParallelOps.threadCount, cps);
+//                                Affinity.setAffinity(bitSet);
+
                                 /*if (ParallelOps.worldProcRank == 1){
                                     try {
                                         System.out.println("Rank: " + ParallelOps.worldProcRank + " thread " + threadIdx +  "  " + threadId + " affinity " + Utils.getProcAffinityMask(threadId));
