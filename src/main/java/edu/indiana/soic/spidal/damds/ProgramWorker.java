0b44450649b70036bc1695cd8606992d2d281bc3
Saliya Ekanayake <esaliya@gmail.com>
2016-07-30 15:51:19 -0400

    debugs

--- src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
@@ -384,6 +384,11 @@ public class ProgramWorker {
 
     private void printTimingDistributions() throws BrokenBarrierException, InterruptedException, MPIException {
         double [] mmInternalTimings = threadComm.gatherDoublesOverThreads(threadId, mmTimings.getTotalTime(MMTimings.TimingTask.MM_INTERNAL));
+        // TODO - debugs
+        if (threadId == 0 && ParallelOps.worldProcRank == 0){
+            System.out.println("**" + Arrays.toString(mmInternalTimings));
+        }
+
         double [] bcInternalTimings = threadComm.gatherDoublesOverThreads(threadId, bcTimings.getTotalTime(BCTimings.TimingTask.BC_INTERNAL));
 
         if (ParallelOps.worldProcsCount > 1 && threadId == 0) {
