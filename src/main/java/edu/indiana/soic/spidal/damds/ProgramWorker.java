49177a9ae3b09426840a2251761ff82d0fc5af57
pulasthi <pulasthi911@gmail.com>
2019-05-22 16:45:09 -0400

    adding code for orignal stress

--- src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
+++ src/main/java/edu/indiana/soic/spidal/damds/ProgramWorker.java
@@ -220,10 +220,10 @@ public class ProgramWorker {
 
                 diffStress = config.threshold + 1.0;
 
-//                utils.printMessage(
-//                        String.format(
-//                                "\nStart of loop %d Temperature (T_Cur) %.5g",
-//                                loopNum, tCur));
+                utils.printMessage(
+                        String.format(
+                                "\nStart of loop %d Temperature (T_Cur) %.5g",
+                                loopNum, tCur));
 
                 int itrNum = 0;
                 cgCount.setValue(0);
@@ -270,20 +270,25 @@ public class ProgramWorker {
                             INV_SUM_OF_SQUARE);
                     stressLoopTimings.endTiming(
                             StressLoopTimings.TimingTask.STRESS);
-
+                    double oriStress = calculateStressOriginal(
+                            preX, config.targetDimension, tCur, distances,
+                            weights,
+                            INV_SUM_OF_SQUARE);
                     diffStress = preStress - stress;
                     preStress = stress;
 
                     if ((itrNum % 10 == 0) || (itrNum >= config.stressIter)) {
-//                        utils.printMessage(
-//                                String.format(
-//                                        "  Loop %d Iteration %d Avg CG count " +
-//                                                "%.5g " +
-//                                                "Stress " +
-//                                                "%.5g", loopNum, itrNum,
-//                                        (cgCount.getValue() * 1.0 / (itrNum +
-//                                                1)),
-//                                        stress));
+                        utils.printMessage(
+                                String.format(
+                                        "  Loop %d Iteration %d Avg CG count " +
+                                                "%.5g " +
+                                                "Stress " +
+                                                "%.5g " +
+                                                "Stress Original" +
+                                                "%.5g", loopNum, itrNum,
+                                        (cgCount.getValue() * 1.0 / (itrNum +
+                                                1)),
+                                        stress, oriStress));
                     }
                     ++itrNum;
                     ++smacofRealIterations;
@@ -1086,6 +1091,94 @@ public class ProgramWorker {
         return refDouble.getValue() * invSumOfSquareDist;
     }
 
+    /**
+     * calculates the stress value as given in equation (1) in
+     * http://dsc.soic.indiana.edu/publications/da_smacof.pdf rather than using
+     * eq (18) as done in the normal stress calc
+     * @param preX
+     * @param targetDimension
+     * @param tCur
+     * @param distances
+     * @param weights
+     * @param invSumOfSquareDist
+     * @return
+     * @throws MPIException
+     * @throws BrokenBarrierException
+     * @throws InterruptedException
+     */
+    private double calculateStressOriginal(
+            double[] preX, int targetDimension, double tCur, short[] distances,
+            WeightsWrap1D weights, double invSumOfSquareDist)
+            throws MPIException, BrokenBarrierException, InterruptedException {
+
+        refDouble.setValue(calculateStressInternalOriginal(threadId, preX,
+                targetDimension, tCur,
+                distances, weights));
+        threadComm.sumDoublesOverThreads(threadId, refDouble);
+        totalCommsTimings.startTiming(TotalCommsTimings.TimingTask.ALL);
+
+        if (ParallelOps.worldProcsCount > 1 && threadId == 0) {
+            totalCommsTimings.startTiming(TotalCommsTimings.TimingTask.BARRIER);
+            ParallelOps.worldProcsComm.barrier();
+            totalCommsTimings.endTiming(TotalCommsTimings.TimingTask.BARRIER);
+
+            totalCommsTimings.startTiming(TotalCommsTimings.TimingTask.COMM);
+            totalCommsTimings.startTiming(TotalCommsTimings.TimingTask.STRESS);
+
+            double stress = refDouble.getValue();
+            // reverting to default MPI call of allreduce<double>
+
+            stress = ParallelOps.allReduce(stress);
+
+            /*
+            // Write thread local reduction to shared memory map
+            ParallelOps.mmapSWriteBytes.position(0);
+            ParallelOps.mmapSWriteBytes.writeDouble(stress);
+
+            // Important barrier here - as we need to make sure writes are done
+            // to the mmap file.
+            // It's sufficient to wait on ParallelOps.mmapProcComm,
+            // but it's cleaner for timings if we wait on the whole world
+            ParallelOps.worldProcsComm.barrier();
+            if (ParallelOps.isMmapLead) {
+                // Node local reduction using shared memory maps
+                ParallelOps.mmapSReadBytes.position(0);
+                stress = 0.0;
+                for (int i = 0; i < ParallelOps.mmapProcsCount; ++i) {
+                    stress += ParallelOps.mmapSReadBytes.readDouble();
+                }
+                ParallelOps.mmapSWriteBytes.position(0);
+                ParallelOps.mmapSWriteBytes.writeDouble(stress);
+
+                // Leaders participate in MPI AllReduce
+                stressTimings.startTiming(StressTimings.TimingTask.COMM, 0);
+                ParallelOps.partialSAllReduce(MPI.SUM);
+                stressTimings.endTiming(StressTimings.TimingTask.COMM, 0);
+            }
+
+            // Each process in a memory group waits here.
+            // It's not necessary to wait for a process
+            // in another memory map group, hence the use of mmapProcComm.
+            // However it's cleaner for any timings to have everyone sync here,
+            // so will use worldProcsComm instead.
+            ParallelOps.worldProcsComm.barrier();
+            ParallelOps.mmapSReadBytes.position(0);
+            stress = ParallelOps.mmapSReadBytes.readDouble();*/
+
+
+            refDouble.setValue(stress);
+            totalCommsTimings.endTiming(TotalCommsTimings.TimingTask.COMM);
+            totalCommsTimings.endTiming(TotalCommsTimings.TimingTask.STRESS);
+
+        }
+
+        // threadComm.barrier();
+        threadComm.bcastDoubleOverThreads(threadId, refDouble, 0);
+        totalCommsTimings.endTiming(TotalCommsTimings.TimingTask.ALL);
+
+        return refDouble.getValue() * invSumOfSquareDist;
+    }
+
     private double calculateStressInternal(
             int threadIdx, double[] preX, int targetDim, double tCur, short[]
             distances, WeightsWrap1D weights) {
@@ -1123,6 +1216,47 @@ public class ProgramWorker {
 
                 heatD = origD - diff;
                 tmpD = origD >= diff ? heatD - euclideanD : -euclideanD;
+                weight = 1.0;
+                sigma += weight * tmpD * tmpD;
+            }
+        }
+        stressInternalTimings.endTiming(StressInternalTimings.TimingTask
+                .COMP, threadIdx);
+        return sigma;
+    }
+
+    private double calculateStressInternalOriginal(
+            int threadIdx, double[] preX, int targetDim, double tCur, short[]
+            distances, WeightsWrap1D weights) {
+
+        stressInternalTimings.startTiming(StressInternalTimings.TimingTask
+                .COMP, threadIdx);
+        double sigma = 0.0;
+        int count = 0;
+        int threadRowCount = globalThreadRowRange.getLength();
+        final int globalRowOffset = globalThreadRowRange.getStartIndex();
+
+        int globalColCount = ParallelOps.globalColCount;
+        int globalRow;
+        double origD, weight, euclideanD;
+        double tmpD;
+        for (int threadLocalRow = 0; threadLocalRow < threadRowCount;
+             ++threadLocalRow) {
+            globalRow = threadLocalRow + globalRowOffset;
+            for (int globalCol = 0; globalCol < globalColCount; globalCol++) {
+                origD = distances[threadLocalRow * globalColCount + globalCol]
+                        * INV_SHORT_MAX;
+                weight = weights.getWeight(threadLocalRow, globalCol);
+//                weight = 1.0;
+
+                if (origD < 0 || weight == 0) {
+                    continue;
+                }
+
+                euclideanD = globalRow != globalCol ? calculateEuclideanDist(
+                        preX, globalRow, globalCol, targetDim) : 0.0;
+
+                tmpD =  origD - euclideanD;
                 weight = 1.0;
                 sigma += weight * tmpD * tmpD;
             }
@@ -1229,7 +1363,7 @@ public class ProgramWorker {
             totalCommsTimings.endTiming(TotalCommsTimings.TimingTask.STATS);
 
         }
-//        threadComm.barrier();
+//        threadComm.barrier();Iteration
         threadComm.bcastDoubleStatisticsOverThreads(threadId,
                 distanceSummary, 0);
         threadComm.bcastIntOverThreads(threadId, refInt, 0);
